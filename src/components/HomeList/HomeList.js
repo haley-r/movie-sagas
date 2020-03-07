d5ba2eda08ad8e5a73b4df8650b13bcb349563be
haley-r <haleyryan77@gmail.com>
2020-03-07 15:23:56 -0600

    turned movie posters into clickable buttons. no functionality yet besides console logging

--- src/components/HomeList/HomeList.js
+++ src/components/HomeList/HomeList.js
@@ -7,14 +7,21 @@ class HomeList extends Component {
         this.props.dispatch({ type: "GET_MOVIES" });
     }
 
+    goToDetails=()=>{
+        console.log('clicked on movie poster');
+    }
+
     render() {
         return (
             <section className="HomeList">
+                <p>click on poster to view movie details</p>
                 <div className="grid-container">
                     {this.props.reduxMovieState.map((movie)=>
-                        <article className="MoviePreview">
+                        <article className="MoviePreview" key={movie.id}>
                             <h2>{movie.title}</h2>
-                            <img src={movie.poster} alt={`movie poster for ${movie.title}`}/>
+                            <button onClick={this.goToDetails} title={`see details about ${movie.title}`}>
+                                <img src={movie.poster} alt={`movie poster for ${movie.title}`}/>
+                            </button>
                         </article>
                     )}
                 </div>
